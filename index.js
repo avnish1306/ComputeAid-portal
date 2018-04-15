ca72f60adb8e781918071d0f9ca9e25cf7985166
Suyash Mittal <suyash8514@gmail.com>
2018-04-16 00:06:44 +0530

    used cors package and added css

--- index.js
+++ index.js
@@ -26,15 +26,7 @@ const chals = require("./routes/chals");
 
 // Middleware Setup
 app.use(morgan('dev'));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-});
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(expressValidator());
