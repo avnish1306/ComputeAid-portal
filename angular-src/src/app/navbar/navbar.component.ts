3c6f54802b9a96b230eee9d8f30de440a337767b
avnish <awi@localhost.localdomain>
2019-03-31 19:48:12 +0530

    pulling 1st time

--- angular-src/src/app/navbar/navbar.component.ts
+++ angular-src/src/app/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faSignOutAlt, faSignInAlt, faUserPlus, faUser, faInfoCircle, faListOl, faSignal } from '@fortawesome/fontawesome-free-solid'
 import fontawesome from '@fortawesome/fontawesome';
-
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { DataService } from '../services/data.service';
 
@@ -16,12 +16,15 @@ fontawesome.library.add(faSignOutAlt, faSignInAlt, faUserPlus, faUser, faInfoCir
 export class NavbarComponent implements OnInit {
 
   constructor(private authService: AuthService,
-    private data: DataService) { }
+    private data: DataService,private router: Router) { }
   
   user: string;
+  contest;
 
   ngOnInit() {
-    this.data.currentName.subscribe(message => this.user = message)
+    this.data.currentName.subscribe(message => this.user = message);
+    this.contest=this.router.url;
+    console.log(this.contest);
   }
 
   isLoggedIn(){
