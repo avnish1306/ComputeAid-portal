843bc671f71c0602a74186f04ec1f9f4fdc8b89e
avnish <awi@localhost.localdomain>
2019-04-01 17:58:32 +0530

    Flawless execution api and file picker done but not tested yet

--- routes/flaw.js
+++ routes/flaw.js
@@ -4,9 +4,12 @@ const router = express.Router();
 const Chal = require('../models/chal');
 const Que=require('../models/que');
 const Flaw=require('../models/flaw');
-const Answer = require('../models/answer');
+const Sub = require('../models/sub');
 const Auth = require('../middlewares/auth');
 const User=require('../models/user');
+const { c,cpp,node,python,java} = require('compile-run');
+
+const negScore=0;
 //Auth.authenticateAll, 
 router.post('/add',Auth.authenticateAll,  (req, res, next) => {
     req.checkBody('qCode', 'Question Code is required').notEmpty();
@@ -27,6 +30,10 @@ router.post('/add',Auth.authenticateAll,  (req, res, next) => {
     const errors = req.validationErrors();
 
     if(!errors){
+        /*let buff=new Buffer(req.body.ipFile.value,'base64');
+        let ipFile=buff.toString('ascii');
+        let buff2=new Buffer(req.body.opFile.value);
+        let opFile=buff2.toString('ascii');*/
         const flaw = new Flaw({
             qCode: req.body.qCode,
             desc: req.body.desc,
@@ -43,6 +50,8 @@ router.post('/add',Auth.authenticateAll,  (req, res, next) => {
             timeLimit: req.body.timeLimit,
             sourceLimit : req.body.sourceLimit
         });
+        
+        
         flaw.save()
         .then(result => {
             res.status(201).json({
@@ -121,7 +130,7 @@ router.get('/', Auth.authenticateAll,  (req, res, next) => {
 });
 
 router.post('/execute',Auth.authenticateAll, function(req, res,next) {
-    fs.writeFile("C:\\Users\\user\\Desktop\\submitfile."+req.body.l,req.body.c, function(err) {
+    /*fs.writeFile("C:\\Users\\user\\Desktop\\submitfile."+req.body.lang,req.body.c, function(err) {
         if(err) {
             return console.log(err);
         }
@@ -131,38 +140,131 @@ router.post('/execute',Auth.authenticateAll, function(req, res,next) {
     //     console.log(input);
     // });
     var input = fs.readFileSync("C:\\Users\\user\\Desktop\\input.txt",'utf8').toString();
-    var output = fs.readFileSync("C:\\Users\\user\\Desktop\\output.txt",'utf8').toString();
-    let resultPromise;
-    if(req.body.l == 'c')
-        resultPromise = c.runFile('C:\\Users\\user\\Desktop\\submitfile.c', { stdin: input, timeout: 1000});
-    if(req.body.l == 'cpp')
-        resultPromise = cpp.runFile('C:\\Users\\user\\Desktop\\submitfile.cpp', { stdin: input, timeout: 1000});
-    if(req.body.l == 'java')
-        resultPromise = java.runFile('C:\\Users\\user\\Desktop\\submitfile.java', { stdin: input, timeout: 1000});
-    if(req.body.l == 'py')
-        resultPromise = python.runFile('C:\\Users\\user\\Desktop\\submitfile.py', { stdin: input, timeout: 1000});
-    resultPromise
-        .then(result => {
-            if(result.errorType == "run-time") {
-                if(result.exitCode == null)
-                  res.status(200).send({res: result, code: 4});
-                if(result.exitCode > 0)
-                  res.status(200).send({res: result, code: 3});
-            }
-            else
-            if(result.errorType == "compile-time") {
-                res.status(200).send({res: result, code: 2});
-            }
-            else
-            if(output == result.stdout)
-              res.status(200).send({res: result, code: 1});
-            else
-              res.status(200).send({res: result, code: 0});
-        })
-        .catch(err => {
-            res.status(200).send(err);
-        });
+    var output = fs.readFileSync("C:\\Users\\user\\Desktop\\output.txt",'utf8').toString();*/
+    Flaw.findOne({'qCode':req.body.qCode},(err,flaw)=>{
+        if(err){
+            res.status(500).json({
+                status: 0,
+                ms:"Question Not Found"
+            });
+        }
+        let ibuff=new Buffer(flaw.ipFile.value,'base64');
+        var input=ibuff.toString('ascii');
+
+        let obuff=new Buffer(flaw.opFile.value,'base64');
+        var output=obuff.toString('ascii');
+
+        let resultPromise;
+        if(req.body.lang == 'c')
+            resultPromise = c.runSource(req.body.code, { stdin: input, timeout: flaw.timeLimit});
+        if(req.body.lang == 'cpp')
+            resultPromise = cpp.runSource(req.body.code, { stdin: input, timeout: flaw.timeLimit});
+        if(req.body.lang == 'java')
+            resultPromise = java.runSource(req.body.code, { stdin: input, timeout: flaw.timeLimit});
+        if(req.body.lang == 'py')
+            resultPromise = python.runSource(req.body.code, { stdin: input, timeout: flaw.timeLimit});
+        resultPromise
+            .then(result => {
+                if(result.errorType == "run-time") {
+                    if(result.exitCode == null)
+                        saveSubmission(req,res,result,4,"TLE",negScore,"JUDGED")
+                    //res.status(200).send({ status:1,res: result, eCode: 4}); //tle
+                    if(result.exitCode > 0)
+                        saveSubmission(req,res,result,3,"RTE",negScore,"JUDGED")//res.status(200).send({ status:1,res: result, eCode: 3});  //rte
+                }
+                else
+                if(result.errorType == "pre-compile-time") {
+                    saveSubmission(req,res,result,21,"PCTE",negScore,"JUDGED")//res.status(200).send({res: result, code: 21});   //cte
+                }
+                else
+                if(result.errorType == "compile-time") {
+                    saveSubmission(req,res,result,2,"CTE",negScore,"JUDGED")//res.status(200).send({res: result, code: 2});   //cte
+                }
+                else
+                if(output == result.stdout)
+                     saveSubmission(req,res,result,1,"AC",flaw.points,"JUDGED")//res.status(200).send({res: result, code: 1});  //ac
+                else
+                    saveSubmission(req,res,result,0,"WA",negScore,"JUDGED")//res.status(200).send({res: result, code: 0});  //wa
+            })
+            .catch(err => {
+                saveSubmission(req,res,null,100,"",negScore,"UNJUDGED")//res.status(500).send(err);
+            });
+    })
+    
 });
+
+function saveSubmission(req,res,result,eCode,eType,score,status){
+    var d=new Date();
+    const sub=new Sub({
+        qCode:req.body.qCode,
+        code:req.body.code,
+        name:req.user.name,
+        lang:req.body.lang,
+        submittedOn:d,
+        result: eType,
+        points:score,
+        status:status,
+        desc: result.stderr
+
+    });
+    sub.save().then(newSub=>{
+        User.findOne({'name':req.user.name},(err,user)=>{
+            if(err){
+                res.status(500).json({
+                    status:0,
+                    msg:"Failed to find in user",
+                    error:err
+        
+                })
+            }
+            var newFlawSubmission= user.flawSubmission.filter(sub=>{
+                return sub.qCode!=req.body.qCode;
+            })
+            var newSub={
+                'qCode':req.body.qCode,
+                'code':req.body.code,
+                'result':eType,
+                'status':status
+            }
+            if(result&&eCode==1){
+                user.contests.flawless.score=user.contests.flawless.score+score;
+            }else if(result){
+                user.contests.flawless.score=user.contests.flawless.score-score;
+            }
+            newFlawSubmission.push(newSub);
+            user.flawSubmission=newFlawSubmission;
+            user.save().then(newUser=>{
+                        res.status(200).json({
+                            status:1,
+                            msg:status,
+                            res:result,
+                            eCode:eCode
+                
+                        })
+                    
+                   
+                
+            }).catch(err=>{
+                res.status(500).json({
+                    status:0,
+                    msg:"Fail to save user",
+                    error:err
+        
+                })
+            })
+            
+        })
+        
+        
+    }).catch(err=>{
+        res.status(500).json({
+            status:1,
+            msg:"Submission not save",
+            error:err
+
+        })
+    })
+}
 
 router.delete('/:id', Auth.authenticateAdmin, (req, res, next) => {
     Que.remove({_id: req.params.id}).exec()
